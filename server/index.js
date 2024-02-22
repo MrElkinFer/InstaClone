8b0bc9d189587894409c45fc21af138a0b236242
Elkin Arismendy <elk.orion@gmail.com>
2024-02-22 16:22:44 -0500

    Configuración final de carpetas

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ require ("dotenv").config({path: ".env"})
 
 console.log(process.env.BBDD)
 
+//Remover //Remover 
+// BBDD=mongodb+srv://instaclone_admin:admin123@atlascluster.0ufzoff.mongodb.net/instaclone
 
 /*mongoose.connect(process.env.BBDD{
    
