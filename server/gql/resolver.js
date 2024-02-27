fbbb5c6ba62b6a148e226077ba170551463e032a
Elkin Arismendy <elk.orion@gmail.com>
2024-02-27 16:00:16 -0500

    primer crud IstaClone

--- server/gql/resolver.js
+++ server/gql/resolver.js
@@ -12,11 +12,33 @@ const resolvers ={
         //Usuario
         Register: async (_,{input}) => {
            const newUser= input;
+           
            newUser.email =newUser.email.toLowerCase();
            newUser.username = newUser.username.toLowerCase();
            
+    
            const {email,username,password} = newUser;
+
+           //revisa si el correo ya está en uso:
            const foundEmail = await User.findOne({email});
+           if(foundEmail) throw new Error("El email ya está en uso");
+
+           //Revisa si el nobre de usuario ya esta en uso:
+           const foundUserName = await User.findOne({username});
+           if(foundUserName) throw new Error("El nombre de usuario ya esta en uso");
+
+           //Espacio para la encriptación de la contraceña:
+
+           try{
+            const user = new User(newUser);
+            user.save();
+            return user;
+
+           }catch(error){
+            console.log(error);
+           }
+
+
            console.log(foundEmail);
            console.log(newUser);
            return null;
