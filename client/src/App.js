2fbaea524947ce56d76ddc2b212f27370324017f
Elkin Arismendy <elk.orion@gmail.com>
2024-02-29 11:07:11 -0500

    Creación cliente e instalación libreria semantic

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from 'semantic-ui-react'
+
+export default function app() {
+  return (
+    <div className="app">
+    <h1>
+      Estamos en App
+    </h1>
+    <Button primary>Primary</Button>
+    <Button secondary>Secondary</Button>
+    </div>
+  );
+}
+
+
