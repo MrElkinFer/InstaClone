c996b8498374df3e8e0c30d0cd746faddc576e04
Elkin Arismendy <elk.orion@gmail.com>
2024-03-01 15:09:52 -0500

    Formulario de registro estética

--- client/src/components/Auth/RegisterForm/RegisterForm.js
+++ client/src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import {Form, Button} from "semantic-ui-react";
+import "./RegisterForm.scss";
 
-export default function RegisterForm() {
+
+export default function RegisterForm(props) {
+
+  const {setShowLogin} = props;
+
+  const onSubmit= () => {console.log("formulario enviado")};
+
   return (
-    <div>
-      <h1>Estamos en RegisterForm</h1>
-    </div>
+    <>
+      <h2 className='register-form-title'>
+        Registrate para ver fotos y videos de tus amigos
+      </h2>
+
+      <Form className='register-form' onSubmit= {onSubmit}>
+        <Form.Input type='text' placeholder= "Nombre y apellidos" name=  "name" /> 
+        <Form.Input type='text' placeholder= "Nombre de usuario" name=  "username" /> 
+        <Form.Input type='text' placeholder= "Correo electrónico" name=  "email" /> 
+        <Form.Input type='password' placeholder= "Contraseña" name=  "password" />
+        <Form.Input type='password' placeholder= "Repetir contraseña" name=  "repeatPassword" /> 
+      
+        <Button type="submit" className='btn-submit'>Registrarse</Button>
+
+      </Form>
+
+    </>
   )
 }
--- client/src/pages/Auth/Auth.js
+++ client/src/pages/Auth/Auth.js
@@ -3,16 +3,17 @@ import "./Auth.scss";
 import {Container, Image} from "semantic-ui-react";
 import instaclone from "../../assets/png/instaclone.png";
 import "./Auth.scss";
+import RegisterForm from '../../components/Auth/RegisterForm';
 
 export default function Auth() {
   
-  const [showLogin, setShowLogin] =useState(false);// true en caso de no tener cuenta y false en caso de tenerla
+  const [showLogin, setShowLogin] =useState(true);// true en caso de no tener cuenta y false en caso de tenerla
   
   return (
     <Container fluid className='auth'>
       <Image src={instaclone}/>
        <div className='container-form'>
-          {!showLogin ? <p>Formulario de Login</p>:<p>Formulario de Registro</p>}
+          {!showLogin ? <p>Formulario de Login</p>:<RegisterForm setShowLogin={setShowLogin}/>}
 
        </div>
 
