47e23599594b4fe252a2e1944bef4772e8705407
Elkin Arismendy <elk.orion@gmail.com>
2024-03-15 08:37:24 -0500

    Panel de usuario básico

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {Grid, Image} from "semantic-ui-react";
 import "./Profile.scss";
 import { useQuery } from '@apollo/client';// para hacer el get de obtener datos
 import { GET_USER } from '../../gql/user';
+import imageNoFound from "../../assets/png/avatar.png"; 
+
 
 
 export default function Profile(props) {
@@ -15,7 +18,7 @@ export default function Profile(props) {
   if(loading)return null; // si no ha cargado no renderiza la pantalla: loading === false
   if(error) return <h1>Usuario no encontrado</h1> 
 
-  const {getUser}= data;
+  const {getUser}= data;// aquí estan los datos del usuario
 
   console.log(getUser);
 
@@ -23,7 +26,32 @@ export default function Profile(props) {
   return (
 
     <>
-      <h1>Profile...</h1>
+      <Grid className='profile'>
+        <Grid.Column width={5} className='profile__left'>
+          <Image src={imageNoFound} avatar />
+        </Grid.Column>
+        <Grid.Column width={11} className='profile__right'>
+          <div>Header Profile</div>
+          <div>Followers</div>
+          <div className='other'>
+            <p className='name'>{getUser.name}</p>
+            
+            {getUser.siteweb && (
+            <a href={getUser.siteweb} className='siteweb'>
+              {getUser.siteweb}
+            </a> 
+            )}
+
+            {getUser.description && (
+              <p className='description'>{getUser.description}</p>
+
+            )}
+           
+           
+         
+          </div>
+        </Grid.Column>
+      </Grid>
     </>
   )
 }
