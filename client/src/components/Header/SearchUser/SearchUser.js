c8a8b78aa20192e94c41c3efff135acd20b21cd0
Elkin Arismendy <elk.orion@gmail.com>
2024-04-11 16:37:24 -0500

    primera parte de la solución

--- client/src/components/Header/SearchUser/SearchUser.js
+++ client/src/components/Header/SearchUser/SearchUser.js
@@ -1,5 +1,6 @@
-import React, {useState}  from 'react'; // importamos también el useState
+import React, {useState, useEffect}  from 'react'; // importamos también el useState
 import "./SearchUser.scss";
+import { size } from 'lodash'; // para ver el tamaño de un array el del useEffect que hacemos.
 import { Search } from 'semantic-ui-react';
 import { useQuery } from '@apollo/client';// traemos la query
 import { SEARCH } from '../../../gql/user'; //traemos la petición de search
@@ -7,19 +8,35 @@ import { SEARCH } from '../../../gql/user'; //traemos la petición de search
 export default function SearchUser() {
    
   //
-    const [searchi, setSearchi] = useState(null)//es mejor inicializar en null para no tener problemas al iniciar con todos los valores: ""
-    console.log(searchi);     // Estado para:
-    const {data,loading} = useQuery(SEARCH, {
-      variables:{ search: searchi},// si la llamo igual que seach no  hay problema y puedo llamarla así no más: "varibles: {search}" pero es mejor que primero entienda que es así.
-
+    const [ search, setSearch ] = useState(null);//es mejor inicializar en null para no tener problemas al iniciar con todos los valores: "" 
+    const [ results, setResults ] = useState([]);
+    const { data, loading } = useQuery(SEARCH, {
+      variables:{ search },// si la llamo igual que seach no  hay problema y puedo llamarla así no más: "varibles: {search}" pero es mejor que primero entienda que es así.
     });
-
-    console.log(data);
+ 
+    console.log(results);
     
-    const onChange = (e)=>{
-      if(e) setSearchi(e.target.value);
-      else setSearchi(null);
-    }
+
+    useEffect(() => {
+      
+      if (size(data?.searchUser) > 0) {
+        const users =[];
+        
+      
+
+      }else{
+        setResults([]);
+        
+      }
+
+    },[data])
+
+    
+    
+    const onChange= (e) => {
+      if(e.target.value){setSearch(e.target.value)}
+      else setSearch(null);
+    };
 
 
   return (
@@ -28,7 +45,8 @@ export default function SearchUser() {
       fluid
       input = { {icon:"search", iconPosition: "left"}}
       onSearchChange={onChange}
+      
     />
-  )
+  );
 }
 
--- client/src/gql/user.js
+++ client/src/gql/user.js
@@ -69,10 +69,7 @@ export const SEARCH = gql`
         searchUser(search: $search) {
             name
             avatar
-            siteweb
-            username
-            email
-            description
+            username            
         }
     }
 `;
