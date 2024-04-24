c8f4d02f815ae81d86f5e44827c8d5bb060f726e
Elkin Arismendy <elk.orion@gmail.com>
2024-04-24 16:43:57 -0500

    Sistema de publicaciones creación de botón publicar imágenes

--- client/src/components/Header/RightHeader/RightHeader.js
+++ client/src/components/Header/RightHeader/RightHeader.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./RightHeader.scss";
 import {Icon, Image} from "semantic-ui-react";
 import useAuth from "../../../hooks/useAuth";
 import imageNoFound from "../../../assets/png/avatar.png";
+import ModalUpload from '../../Modal/ModalUpload/ModalUpload'; // vid 132
 //import { Link } from 'react-router-dom';
 
 export default function RightHeader() {
+  const[showModal, setShowModal]= useState(false);
   const {auth} = useAuth();
-  console.log(auth);
+  //console.log(auth);
   
   return (
-    <div className='right-header'>
-        <a href='/'>
-            <Icon name='home'/>
-        </a>
-        <a href ='/'>
-            <Icon name='plus'/>
-        </a>
-        <a href={auth.username} >
-          <Image src={imageNoFound} avatar />
-        </a>
-        
-    </div>
+    <>
+      <div className='right-header'>
+          <a href='/'>
+              <Icon name='home'/>
+          </a>
+          
+          <Icon name='plus' onClick = {() => setShowModal(true)}/>
+          
+          <a href={auth.username} >
+            <Image src={imageNoFound} avatar/>
+          </a>
+      </div>
+      
+      <ModalUpload show = {showModal} setShow ={setShowModal}/>
+    </>
   )
 }
--- server/utils/aws-upload-image.js
+++ server/utils/aws-upload-image.js
@@ -1,9 +1,9 @@
 require("dotenv").config({path: ".env"});
-const AWS = require("aws-sdk");
+const AWS = require("aws-sdk"); 
 
 const ID = process.env.AWS_ID;
-const SECRET = process.AWS_SECRET;
-const BUCKET_NAME = process.AWS_BUCKET_NAME;
+const SECRET = process.env.AWS_SECRET;
+const BUCKET_NAME = process.env.AWS_BUCKET_NAME;
 
 const s3 = new AWS.S3({
     accessKeyId: ID,
