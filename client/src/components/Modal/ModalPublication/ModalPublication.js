c523fc7d2a3e2b2ef9eeac3e67ab7a5b89778323
Elkin Arismendy <elk.orion@gmail.com>
2024-05-01 17:25:23 -0500

    modal de la publicación

--- client/src/components/Modal/ModalPublication/ModalPublication.js
+++ client/src/components/Modal/ModalPublication/ModalPublication.js
@@ -3,10 +3,18 @@ import { Modal, Grid } from 'semantic-ui-react';
 import './ModalPublication.scss';
 
 export default function ModalPublication(props) {
-    const {show, setshow, publication} = props;
+    const {show, setShow, publication} = props;
+    console.log(publication);
+
+    const onClose = () => setShow(false);
+
+
   return (
-    <Modal open={show} onClose={setshow} className='modal-publication '>
-        <h1>modal</h1>
+    <Modal open={show} onClose={onClose} className='modal-publication '>
+        <Grid>
+          <Grid.Column className='modal-publication__left'width={10} style={{backgroundImage: `url("${publication.file}")`}}/>
+          <Grid.Column className='modal-publication__right' width={6}>COMENTARIOS  </Grid.Column>
+        </Grid>
     </Modal>
   )
 }
